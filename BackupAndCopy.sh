0bb00df27fad6332f2468b5b8f2f5e25d2a9953e
SubstituteUser <dcheng1@luc.edu>
2018-04-01 13:15:42 -0500

    Needed super user priviledges in BackupAndCopy.sh to write to /etc

--- BackupAndCopy.sh
+++ BackupAndCopy.sh
@@ -1,7 +1,8 @@
 mv ~/.vimrc ~/.vimrc.bak;
 cp ~/.bashrc ~/.bashrc.original.bak
+
 # I personally prefer RHEL's way of doing .bashrc
 # So I mimic that in Ubuntu
-
-mv ~/.bashrc ~/etc/bashrc;
+# writing to /etc will need super user priviledge
+sudo mv ~/.bashrc ~/etc/bashrc;
 cp .vimrc .bashrc ~/;
